dd0087f9edb3baac42531e3706b4ccbf56002856
Tetyana Gogulya <cheburashka077@ukr.net>
2024-11-03 00:27:50 +0200

    terms_of_service

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,29 +1,37 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const modalNav = document.querySelector('#modal-nav');
-const closeModal = document.querySelector('.close-modal');
-const header = document.querySelector('.header');
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const modalNav = document.querySelector('#modal-nav');
+  const closeModal = document.querySelector('.close-modal');
+  const headerContent = document.querySelector('.header-content');
 
-// Функція для відкриття модального меню і зміни кольору хедера
-menuToggle.addEventListener('click', event => {
-  event.stopPropagation();
-  modalNav.classList.toggle('open');
-  header.classList.toggle('blue-bg', modalNav.classList.contains('open'));
-});
+  if (menuToggle && modalNav && closeModal && headerContent) {
+    menuToggle.addEventListener('click', event => {
+      event.stopPropagation();
+      modalNav.classList.toggle('open');
+      headerContent.classList.toggle(
+        'header-content-blue-bg',
+        modalNav.classList.contains('open')
+      );
+    });
 
-// Закриває модальне вікно та змінює колір хедера на стандартний
-closeModal.addEventListener('click', () => {
-  modalNav.classList.remove('open');
-  header.classList.remove('blue-bg');
-});
+    closeModal.addEventListener('click', () => {
+      modalNav.classList.remove('open');
+      headerContent.classList.remove('header-content-blue-bg');
+    });
 
-// Закриття при кліку за межами модального вікна
-document.addEventListener('click', event => {
-  if (
-    modalNav.classList.contains('open') &&
-    !modalNav.contains(event.target) &&
-    event.target !== menuToggle
-  ) {
-    modalNav.classList.remove('open');
-    header.classList.remove('blue-bg');
+    document.addEventListener('click', event => {
+      if (
+        modalNav.classList.contains('open') &&
+        !modalNav.contains(event.target) &&
+        event.target !== menuToggle
+      ) {
+        modalNav.classList.remove('open');
+        headerContent.classList.remove('header-content-blue-bg');
+      }
+    });
+  } else {
+    console.warn(
+      'Not all items are found on this page. Check the HTML structure.'
+    );
   }
 });
