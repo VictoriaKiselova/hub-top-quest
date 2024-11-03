c0493b8c145b99aa29abd7a6ed4a048d38ce0a72
Tetyana Gogulya <cheburashka077@ukr.net>
2024-11-03 13:40:01 +0200

    Update burger-menu.js

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,9 +1,59 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const menuToggle = document.querySelector('.menu-toggle');
+//   const modalNav = document.querySelector('#modal-nav');
+//   const closeModal = document.querySelector('.close-modal');
+//   const headerContent = document.querySelector('.header-content');
+//   const navLinks = document.querySelectorAll('.nav-link a');
+
+//   if (menuToggle && modalNav && closeModal && headerContent) {
+//     menuToggle.addEventListener('click', event => {
+//       event.stopPropagation();
+//       modalNav.classList.toggle('open');
+//       headerContent.classList.toggle(
+//         'header-content-blue-bg',
+//         modalNav.classList.contains('open')
+//       );
+//     });
+
+//     closeModal.addEventListener('click', () => {
+//       modalNav.classList.remove('open');
+//       headerContent.classList.remove('header-content-blue-bg');
+//     });
+
+//     navLinks.forEach(link => {
+//       link.addEventListener('click', () => {
+//         modalNav.classList.remove('open');
+//         headerContent.classList.remove('header-content-blue-bg');
+//       });
+//     });
+
+//     document.addEventListener('click', event => {
+//       if (
+//         modalNav.classList.contains('open') &&
+//         !modalNav.contains(event.target) &&
+//         event.target !== menuToggle
+//       ) {
+//         modalNav.classList.remove('open');
+//         headerContent.classList.remove('header-content-blue-bg');
+//       }
+//     });
+//   } else {
+//     console.warn(
+//       'Not all items are found on this page. Check the HTML structure.'
+//     );
+//   }
+// });
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.querySelector('.menu-toggle');
   const modalNav = document.querySelector('#modal-nav');
   const closeModal = document.querySelector('.close-modal');
   const headerContent = document.querySelector('.header-content');
   const navLinks = document.querySelectorAll('.nav-link a');
+
+  function closeModalNav() {
+    modalNav.classList.remove('open');
+    headerContent.classList.remove('header-content-blue-bg');
+  }
 
   if (menuToggle && modalNav && closeModal && headerContent) {
     menuToggle.addEventListener('click', event => {
@@ -15,16 +65,10 @@ document.addEventListener('DOMContentLoaded', function () {
       );
     });
 
-    closeModal.addEventListener('click', () => {
-      modalNav.classList.remove('open');
-      headerContent.classList.remove('header-content-blue-bg');
-    });
+    closeModal.addEventListener('click', closeModalNav);
 
     navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-        modalNav.classList.remove('open');
-        headerContent.classList.remove('header-content-blue-bg');
-      });
+      link.addEventListener('click', closeModalNav);
     });
 
     document.addEventListener('click', event => {
@@ -33,8 +77,13 @@ document.addEventListener('DOMContentLoaded', function () {
         !modalNav.contains(event.target) &&
         event.target !== menuToggle
       ) {
-        modalNav.classList.remove('open');
-        headerContent.classList.remove('header-content-blue-bg');
+        closeModalNav();
+      }
+    });
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth >= 1200) {
+        closeModalNav();
       }
     });
   } else {
