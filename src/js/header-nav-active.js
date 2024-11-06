bfba006f2476cf0468f136f57eb9fe7c69cad09f
Tetyana Gogulya <cheburashka077@ukr.net>
2024-11-06 23:08:25 +0200

    corr-js-header

--- src/js/header-nav-active.js
+++ src/js/header-nav-active.js
@@ -1,40 +1,73 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const headerOffset = 120;
+//   const sections = document.querySelectorAll('section');
+//   const navLinks = document.querySelectorAll('.nav-link a');
+
+//   function scrollToSectionOnClick(event) {
+//     const targetId = event.currentTarget.getAttribute('href').slice(1);
+//     const targetSection = document.getElementById(targetId);
+
+//     if (targetSection) {
+//       event.preventDefault();
+//       window.scrollTo({
+//         top: targetSection.offsetTop - headerOffset,
+//         behavior: 'smooth',
+//       });
+//     }
+//   }
+
+//   navLinks.forEach(link => {
+//     link.addEventListener('click', scrollToSectionOnClick);
+//   });
+
+//   function changeActiveLink() {
+//     let scrollPosition =
+//       document.documentElement.scrollTop || document.body.scrollTop;
+
+//     sections.forEach(section => {
+//       const sectionTop = section.offsetTop;
+//       const sectionHeight = section.clientHeight;
+
+//       if (
+//         scrollPosition >= sectionTop - headerOffset &&
+//         scrollPosition < sectionTop + sectionHeight - headerOffset
+//       ) {
+//         const id = section.getAttribute('id');
+//         navLinks.forEach(link => {
+//           link.classList.remove('active');
+//           if (link.getAttribute('href') === `#${id}`) {
+//             link.classList.add('active');
+//           }
+//         });
+//       }
+//     });
+//   }
+
+//   window.addEventListener('scroll', changeActiveLink);
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
-  const headerOffset = 120;
   const sections = document.querySelectorAll('section');
   const navLinks = document.querySelectorAll('.nav-link a');
-
-  function scrollToSectionOnClick(event) {
-    const targetId = event.currentTarget.getAttribute('href').slice(1);
-    const targetSection = document.getElementById(targetId);
-
-    if (targetSection) {
-      event.preventDefault();
-      window.scrollTo({
-        top: targetSection.offsetTop - headerOffset,
-        behavior: 'smooth',
-      });
-    }
-  }
-
-  navLinks.forEach(link => {
-    link.addEventListener('click', scrollToSectionOnClick);
-  });
 
   function changeActiveLink() {
     let scrollPosition =
       document.documentElement.scrollTop || document.body.scrollTop;
+
+    const windowHeight = window.innerHeight;
 
     sections.forEach(section => {
       const sectionTop = section.offsetTop;
       const sectionHeight = section.clientHeight;
 
       if (
-        scrollPosition >= sectionTop - headerOffset &&
-        scrollPosition < sectionTop + sectionHeight - headerOffset
+        scrollPosition >= sectionTop - windowHeight / 3 &&
+        scrollPosition < sectionTop + sectionHeight - windowHeight / 3
       ) {
         const id = section.getAttribute('id');
         navLinks.forEach(link => {
           link.classList.remove('active');
+
           if (link.getAttribute('href') === `#${id}`) {
             link.classList.add('active');
           }
@@ -42,6 +75,18 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   }
-
   window.addEventListener('scroll', changeActiveLink);
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      const targetId = link.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      window.scrollTo({
+        top: targetSection.offsetTop - window.innerHeight / 4,
+        behavior: 'smooth',
+      });
+    });
+  });
 });
